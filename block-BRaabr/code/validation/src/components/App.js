86790edfff6cde105c9724a0b403c65c7c41f16f
Vasant Saraswat <vasant.saraswat@gmail.com>
2022-05-23 18:15:50 +0530

    Assignment of form validation is completed

---
+++ block-BRaabr/code/validation/src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Form from './Form';
+import Registration from './Registration';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <>
+        <Form />
+        <Registration />
+      </>
+    );
+  }
+}
+
+export default App;
